0507ce02f121a6493a3c0b88e1863ba68e4dec29
yashdeep940 <Yashdeep.23466@stu.upes.ac.in>
2026-02-11 22:52:44 +0530

    Q11 - Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.

---
+++ Day11.c
@@ -0,0 +1,50 @@
+// Q11-Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
+// Input:
+// - First line: two integers m and n representing the number of rows and columns
+// - Next m lines: n integers each representing the elements of the first matrix
+// - Next m lines: n integers each representing the elements of the second matrix
+
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int A[rows][cols], B[rows][cols], Sum[rows][cols];
+
+    printf("Enter elements of first matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    printf("Sum of matrices:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d ", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
