eac776fb1b21d6c2100f2723fbd581b7853d0b94
yashdeep940 <Yashdeep.23466@stu.upes.ac.in>
2026-05-05 12:32:48 +0530

    q44

---
+++ Day44.c
@@ -0,0 +1,94 @@
+/*Q44-Perform inorder, preorder, and postorder traversals of a given binary tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void inorder(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* nodes[n];
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(nodes[i] != NULL) {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    struct Node* root = nodes[0];
+
+    inorder(root);
+    printf("\n");
+
+    preorder(root);
+    printf("\n");
+
+    postorder(root);
+    printf("\n");
+
+    return 0;
+}
